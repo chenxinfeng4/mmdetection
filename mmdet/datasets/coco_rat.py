33adee52c1b839d793c0c6e2add22796c8b7f2a8
chenxinfeng <chenxinfeng@cibr.ac.cn>
2025-08-15 17:37:12 +0800

    update

--- mmdet/datasets/coco_rat.py
+++ mmdet/datasets/coco_rat.py
@@ -13,3 +13,16 @@ class CocoDatasetRatOneclass(CocoDataset):
 @DATASETS.register_module()
 class CocoDatasetRatBWD(CocoDataset):
     CLASSES = ('rat_black', 'rat_white', 'rat_dot')
+
+@DATASETS.register_module()
+class CocoDatasetRatBWDG(CocoDataset):
+    CLASSES = ('rat_white','rat_gray','rat_black','rat_dot')
+
+@DATASETS.register_module()
+class CocoDatasetRatBWDR(CocoDataset):
+    CLASSES = ('rat_black','rat_white','rat_dot','rat_row')
+
+@DATASETS.register_module()
+class CocoDatasetMar(CocoDataset):
+    CLASSES = ('mar')
+    
